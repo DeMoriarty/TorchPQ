b16ef34e278c80eef4714ea5234a17724bbe8cda
DeMoriarty <sn591771872@vip.qq.com>
2021-02-11 17:26:19 +0300

    add sq to init

--- torchpq/SQ.py
+++ torchpq/SQ.py
@@ -1,3 +1,6 @@
+import torch
+import torch.nn as nn
+
 class SQ(nn.Module):
   """
   scalar qantizer
--- torchpq/__init__.py
+++ torchpq/__init__.py
@@ -2,18 +2,14 @@ try:
   import cupy as cp
 except ModuleNotFoundError:
   raise ModuleNotFoundError("cupy is not installed, please visit https://pypi.org/project/cupy/")
-import torch
-import torch.nn as nn
-import numpy as np
-import math
-from time import time
 
 from . import kmeans
 from . import kernels
 
+from .PQ import PQ
+from .SQ import SQ
 from .IVFPQBase import IVFPQBase
 from .IVFPQTopk import IVFPQTopk
-from .PQ import PQ
 
 from .IVFPQ import IVFPQ
 from .IVFPQR import IVFPQR
