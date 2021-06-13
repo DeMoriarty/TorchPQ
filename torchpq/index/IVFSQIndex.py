90688c22efca8d8d37d3512c8de7762199553459
DeMoriarty <sn591771872@vip.qq.com>
2021-06-14 01:08:48 +0300

    added __init__.py to torchpq/index/

---
+++ torchpq/index/IVFSQIndex.py
@@ -0,0 +1,23 @@
+from .BaseIndex import BaseIndex
+
+class IVFSQIndex(BaseIndex):
+  def __init__(self):
+    super(IVFSQIndex, self).__init__()
+
+  def train(self, x):
+    pass
+  
+  def encode(self, x):
+    pass
+
+  def decode(self, x):
+    pass
+  
+  def add(self, x, ids=None):
+    pass
+
+  def remove(self, ids=None, address=None):
+    pass
+
+  def search(self, queries, k=1):
+    pass
---
+++ torchpq/index/__init__.py
@@ -0,0 +1,6 @@
+from .FlatIndex import FlatIndex
+from .IVFFlatIndex import IVFFlatIndex
+from .IVFSQIndex import IVFSQIndex
+from .IVFPQIndex import IVFPQIndex
+from .IVFPQRIndex import IVFPQRIndex
+from .PQIndex import PQIndex
