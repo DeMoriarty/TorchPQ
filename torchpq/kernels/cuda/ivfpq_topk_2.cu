33070d1d80ad0efc5bae6f26f9856a37c03f9ec7
DeMoriarty <sn591771872@vip.qq.com>
2021-06-23 22:15:03 +0300

    small fix

--- torchpq/kernels/cuda/ivfpq_topk_2.cu
+++ torchpq/kernels/cuda/ivfpq_topk_2.cu
@@ -914,11 +914,10 @@ __global__ void ivfpq_topk_residual_precomputed(
     int nIter = (cCellSize + _TPB_ - 1) / _TPB_;
     for (int iter = 0; iter < nIter; iter++ ){
       int iN = cCellStart + iter * _TPB_ + tid;
-      float value;
+      float value = baseSims[qid * nProbe + cCell];
       float index = iN;
       int cIsEmpty = 0;
       if (cCellStart <= iN && iN < cCellEnd){
-        value = baseSims[qid * nProbe + cCell];
         cIsEmpty = isEmpty[iN];
         uint8n_t dataCache[_M_ / _NCS_];
         load_data(data, dataCache, iN, nData);
