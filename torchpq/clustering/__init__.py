72c11706c93d2826950f385de3ffe2bd68c238b5
DeMoriarty <sn591771872@vip.qq.com>
2021-07-09 14:47:07 +0300

    add MinibatchKMeans

--- torchpq/clustering/__init__.py
+++ torchpq/clustering/__init__.py
@@ -2,3 +2,4 @@ from .KMeans import KMeans
 from .MultiKMeans import MultiKMeans
 from .KMeansOld import KMeansOld
 from .MultiKMeansOld import MultiKMeansOld
+from .MinibatchKMeans import MinibatchKMeans
